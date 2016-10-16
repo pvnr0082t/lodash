ca083fbd3f4045085c9c67b353e6844c1f890d14
John-David Dalton <john.david.dalton@gmail.com>
2016-10-15 19:57:17 -0700

    Bump to v4.16.5.

--- isFunction.js
+++ isFunction.js
@@ -1,19 +1,9 @@
-define(['./isObject'], function(isObject) {
+define(['./_baseGetTag', './isObject'], function(baseGetTag, isObject) {
 
   /** `Object#toString` result references. */
   var funcTag = '[object Function]',
       genTag = '[object GeneratorFunction]',
       proxyTag = '[object Proxy]';
-
-  /** Used for built-in method references. */
-  var objectProto = Object.prototype;
-
-  /**
-   * Used to resolve the
-   * [`toStringTag`](http://ecma-international.org/ecma-262/7.0/#sec-object.prototype.tostring)
-   * of values.
-   */
-  var objectToString = objectProto.toString;
 
   /**
    * Checks if `value` is classified as a `Function` object.
@@ -33,9 +23,12 @@ define(['./isObject'], function(isObject) {
    * // => false
    */
   function isFunction(value) {
+    if (!isObject(value)) {
+      return false;
+    }
     // The use of `Object#toString` avoids issues with the `typeof` operator
-    // in Safari 9 which returns 'object' for typed array and other constructors.
-    var tag = isObject(value) ? objectToString.call(value) : '';
+    // in Safari 9 which returns 'object' for typed arrays and other constructors.
+    var tag = baseGetTag(value);
     return tag == funcTag || tag == genTag || tag == proxyTag;
   }
 
